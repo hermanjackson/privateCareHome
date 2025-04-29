00cd0ed7d1ff44cd3150cd86fbe4e3d860065e13
Herman Jackson <hermanjackson561@gmail.com>
2025-04-29 12:20:44 -0400

    chaned log in

--- src/front/Login.js
+++ src/front/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch('${process.env.REACT_APP_API_URL}/Login', {
+      const response = await fetch('${process.env.REACT_APP_API_URL}/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
